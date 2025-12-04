a966654d3726cd87a57a9d764286573e8e5545d8
Alan Luna <alan.luna@biyiud.eco>
2025-12-04 13:23:53 +0100

    feat(ui): add spinner component

---
+++ src/components/ui/spinner.tsx
@@ -0,0 +1,14 @@
+import { Loader2Icon } from "lucide-react";
+import { cn } from "@/lib/utils"
+import type { ComponentProps } from "react";
+
+export function Spinner({ className, ...props }: ComponentProps<"svg">) {
+  return (
+    <Loader2Icon
+      role="status"
+      aria-label="Loading"
+      className={cn("size-4 animate-spin", className)}
+      {...props}
+    />
+  )
+}
